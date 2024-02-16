93e712e55b57ddd1c233697a8235620cfefc681b
GeorgevirSingh <georgeveersingh27@gmail.com>
2024-02-15 20:00:23 -0500

    Commit

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ app.get('/hello', function (req, res) {
     res.send('<h1>Hello Login</h1>')
   })
 
+// http://localhost:3000/name
+app.get('/name', function (req, res) {
+  res.send('<h1>Hello - C0866872 -  Georgevir Singh</h1> ')
+})
+
 app.listen(SERVER_PORT ,() => {
 	console.log(`Server Running at http://localhost:${SERVER_PORT}/`)	
 }) 
